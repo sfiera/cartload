fe083604a610c419628f790f2b339a2cb6a5bd1a
Chris Pickel <sfiera@twotaled.com>
2026-03-11 20:50:32 -0400

    Add GBA logo decompression

--- src/agb.js
+++ src/agb.js
@@ -3,27 +3,108 @@
 import cmds from "./gbxcart/cmds.js";
 import vars from "./gbxcart/vars.js";
 import {pack, unpack} from "./struct.js";
-import {arrayEq, ints, latin1, makeImage, Segment} from "./util.js";
+import {arrayEq, latin1, makeImage, unhex} from "./util.js";
+
+const nintendoLogo = unhex(
+    "24ffae51699aa2213d84820a84e409ad11248b98c0817f21a352" +
+    "be199309ce2010464a4af82731ec58c7e83382e3cebf85f4df94" +
+    "ce4b09c194568ac01372a7fc9f844d73a3ca9a615897a327fc03" +
+    "9876231dc7610304ae56bf38840040a70efdff52fe036f9530f1" +
+    "97fbc08560d68025a963be03014e38e2f9a234ffbb3e03447800" +
+    "90cb88113a9465c07c6387f03cafd625e48b380aac7221d4f807")
 
 class AgbCart {
   constructor(data, romSize) {
     this.header = data;
+    this.logo = data.slice(0x004, 0x0A0);
     this.title = latin1.decode(data.slice(0x0A0, 0x0AC));
     this.code = latin1.decode(data.slice(0x0AC, 0x0B0));
     this.romSize = romSize;
     this.savSize = 0;
+
+    this.valid = {
+      logo: arrayEq(this.logo, nintendoLogo),
+      headerCksum:
+          data[0x0BD] == data.slice(0x0A0, 0x0BD).reduce((cksum, x) => (cksum - x) & 0xff, 0xe7),
+    };
+    this.valid.header = this.valid.logo && this.valid.headerCksum;
   }
 
   get mapperName() { return "None" }
 
   get extension() { return "gba"; }
 
-  logoImageUrl(header) {
-    return makeImage(64, 8, (ctx) => {
-      ctx.fillStyle = "black";
-      ctx.fillRect(0, 0, 64, 8);
-    });
+  // Translated from FlashGBX code originally by Winter1760
+  drawImage(ctx) {
+    ctx.fillStyle = "black";
+
+    const decompress = (data) => {
+      const bits = data[0] & 0x0F;
+      const outLen = data[1] | (data[2] << 8);
+      let nodeOffs = 5, outUnits = 0, outReady = 0;
+      const out = [];
+
+      for (let i = 6 + data[4] * 2;; i += 4) {
+        const inUnit =
+            data[i] | (data[i + 1] << 8) | (data[i ^ 2] << 16) | (data[(i ^ 2) + 1] << 24)
+        for (let b = 31; b >= 0; b -= 1) {
+          const node = data[nodeOffs];
+          nodeOffs &= 0xFFFFFFFE;
+          nodeOffs += (node & 0x3F) * 2 + 2 + ((inUnit >>> b) & 1);
+          if (node << ((inUnit >>> b) & 1) & 0x80) {
+            outReady >>>= bits;
+            outReady |= (data[nodeOffs] & ((1 << bits) - 1)) << (32 - bits);
+            outUnits += 1;
+            if (outUnits == bits % 8 + 4) {
+              out.push(...pack("I", outReady).toReversed());
+              if (out.length >= outLen) {
+                data.splice(0, data.length, ...out);
+                return;
+              }
+              outUnits = outReady = 0;
+            }
+            nodeOffs = 5;
+          }
+        }
+      }
+    };
+
+    const undiff = (data) => {
+      const outLen = data[1] | (data[2] << 8);
+      let pos = 4;
+      let prev = 0;
+      while (pos < outLen) {
+        if (pos + 2 > data.length) {
+          break;
+        }
+        const next = (data[pos] + (data[pos + 1] << 8) + prev) & 0xFFFF;
+        data.splice(pos, 2, ...pack("H", next).toReversed());
+        pos += 2;
+        prev = next;
+      }
+    };
+
+    let prefix = unhex("24D400000F4000000001818282830F830CC30383018304C3080E02C20DC2070B060A0509");
+    let data = [...prefix];
+    data.push(...this.logo);
+    decompress(data);
+    undiff(data);
+
+    for (let ty = 0; ty < 2; ty += 1) {
+      for (let tx = 0; tx < 13; tx += 1) {
+        for (let x = 0; x < 8; x += 1) {
+          for (let y = 0; y < 8; y += 1) {
+            const pos = (ty * 13 * 8) + (tx * 8) + x + 4;
+            if ((pos < data.length) && (data[pos] & (1 << y))) {
+              ctx.fillRect(tx * 8 + y, ty * 8 + x, 1, 1);
+            }
+          }
+        }
+      }
+    }
   }
+
+  logoImageUrl(header) { return makeImage(104, 16, this.drawImage); }
 
   async backUpRom(client, callback) {
     await client.command(cmds.CART_PWR_ON);
---
+++ src/agb.test.js
@@ -0,0 +1,90 @@
+// Cartload is (c) 2026 by sfiera. Licensed under GPLv3.
+
+import * as agb from "./agb.js";
+import cmds from "./gbxcart/cmds.js";
+import vars from "./gbxcart/vars.js";
+import {unhex} from "./util.js";
+
+const logoBits = unhex(
+    "24ffae51699aa2213d84820a84e409ad11248b98c0817f21a352" +
+    "be199309ce2010464a4af82731ec58c7e83382e3cebf85f4df94" +
+    "ce4b09c194568ac01372a7fc9f844d73a3ca9a615897a327fc03" +
+    "9876231dc7610304ae56bf38840040a70efdff52fe036f9530f1" +
+    "97fbc08560d68025a963be03014e38e2f9a234ffbb3e03447800" +
+    "90cb88113a9465c07c6387f03cafd625e48b380aac7221d4f807")
+
+const logoGfx = [
+  "                                                ▗▄▖ ",
+  " ██▖  ██ ██        ▄▄                  ██      ▗▚▄▝▖",
+  " ██▙  ██ ▀▀       ▄██▄                 ██      ▐▐▄▘▌",
+  " ██▜▌ ██ ██ ██▟█▙▖▀██▀ ▟▛▜▙ ▐█▙██▄  ▟█▙██ ▗█▀█▖▝▞ ▚▘",
+  " ██▝█▖██ ██ ██▘ ██ ██ ▟█  █▙▐█▛ ▐█▌▟█▘ ██▗█▌ ▐█▖▝▀▘ ",
+  " ██ ▐▙██ ██ ██  ██ ██ ██▀▀▀▀▐█▌ ▐█▌██  ██▐█▌ ▐█▌    ",
+  " ██  ▜██ ██ ██  ██ ██ ▜█  ██▐█▌ ▐█▌▜█  ██▝█▌ ▐█▘    ",
+  " ██  ▝██ ██ ██  ██ ██  ▜▙▟▛▘▐█▌ ▐█▌ ▜▙▟██ ▝█▄█▘     ",
+].join("\n");
+
+const tiles = " ▘▝▀▖▌▞▛▗▚▐▜▄▙▟█";  // bitfield
+
+test("decompress logo", async () => {
+  const data = Array(0x180).fill(0);
+  data.splice(0x004, logoBits.length, ...logoBits);
+  const cart = await agb.detect(new FakeClient(data));
+  const logo = Array(8).fill(0).map(_ => Array(52).fill(0));
+  const ctx = {
+    fillRect: (x, y, w, h) => {
+      expect(w).toEqual(1);
+      expect(h).toEqual(1);
+      expect(ctx.fillStyle).toEqual("black");
+      const bit = (x & 1) | ((y & 1) << 1);
+      logo[y >>> 1][x >>> 1] |= 1 << bit;
+    },
+  };
+  cart.drawImage(ctx);
+  expect(logo.map(row => row.map(x => tiles[x]).join("")).join("\n")).toEqual(logoGfx);
+});
+
+class FakeClient {
+  constructor(rom) {
+    this.address = 0;
+    this.rom = new Uint8Array(rom);
+  }
+
+  read(addr) {
+    if (0 <= addr && addr < this.rom.length) {
+      return this.rom[addr] || 0;
+    }
+    return 0xFF;
+  }
+
+  write(addr, value) {}
+
+  async command(cmd, ...args) {
+    if (cmd === cmds.CART_PWR_ON) {
+    } else if (cmd === cmds.CART_PWR_OFF) {
+    } else if (cmd === cmds.ENABLE_PULLUPS) {
+    } else if (cmd === cmds.DISABLE_PULLUPS) {
+    } else {
+      expect(cmd).toEqual(null);
+    }
+  }
+
+  async setVariable(variable, value) {
+    if (variable === vars.ADDRESS) {
+      this.address = value & 0xFFFFFFFF;
+    } else if (variable === vars.CART_MODE) {
+    } else {
+      expect(variable).toEqual(null);
+    }
+  }
+
+  async transfer(cmd, size, callback, ...args) {
+    expect(cmd.id).toBe(cmds.AGB_CART_READ.id);
+    expect(args).toHaveLength(0);
+    const result = new Uint8Array(size);
+    for (let i = 0; i < size; ++i) {
+      result[i] = this.read(this.address++);
+    }
+    return result;
+  }
+}

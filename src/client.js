d9984ff8561f5d11709f8cf8a07485b8b362895b
Chris Pickel <sfiera@twotaled.com>
2026-03-20 08:06:01 -0400

    Serialize async calls to Client

--- src/client.js
+++ src/client.js
@@ -12,6 +12,30 @@ export default class Client {
     this.port = port;
     this.reader = port.readable.getReader();
     this.writer = port.writable.getWriter();
+    this.working = false;
+    this.queue = [];
+  }
+
+  async #work() {
+    while (!this.working && this.queue.length) {
+      const {resolve, reject, fn} = this.queue.shift();
+      this.working = true;
+      try {
+        const result = await fn();
+        resolve(result);
+      } catch (e) {
+        reject(e);
+      } finally {
+        this.working = false;
+      }
+    }
+  }
+
+  #serialize(fn) {
+    const {promise, resolve, reject} = Promise.withResolvers();
+    this.queue.push({resolve, reject, fn});
+    this.#work();
+    return promise;
   }
 
   static async open(port) {
@@ -25,7 +49,7 @@ export default class Client {
     await this.port.close();
   }
 
-  async command(cmd, ...args) {
+  async #command(cmd, ...args) {
     await this.writer.write(pack(cmd.reqFormat, cmd.id, ...args));
     if (!cmd.respFormat.length) {
       return [];
@@ -47,12 +71,12 @@ export default class Client {
     }
   }
 
-  async transfer(cmd, size, callback, ...args) {
+  async #transfer(cmd, size, callback, ...args) {
     let result = [];
     if (size >= MAX_TRANSFER_SIZE) {
-      await this.setVariable(vars.TRANSFER_SIZE, MAX_TRANSFER_SIZE);
+      await this.#setVariable(vars.TRANSFER_SIZE, MAX_TRANSFER_SIZE);
       while (size >= MAX_TRANSFER_SIZE) {
-        await this.transferChunk(cmd, result, MAX_TRANSFER_SIZE, ...args);
+        await this.#transferChunk(cmd, result, MAX_TRANSFER_SIZE, ...args);
         size -= MAX_TRANSFER_SIZE;
         if (callback) {
           callback(result.length);
@@ -60,8 +84,8 @@ export default class Client {
       }
     }
     if (size > 0) {
-      await this.setVariable(vars.TRANSFER_SIZE, size);
-      await this.transferChunk(cmd, result, size, ...args);
+      await this.#setVariable(vars.TRANSFER_SIZE, size);
+      await this.#transferChunk(cmd, result, size, ...args);
       if (callback) {
         callback(result.length);
       }
@@ -69,8 +93,8 @@ export default class Client {
     return result;
   }
 
-  async transferChunk(cmd, result, size, ...args) {
-    await this.command(cmd, ...args);
+  async #transferChunk(cmd, result, size, ...args) {
+    await this.#command(cmd, ...args);
     while (size > 0) {
       let data = (await this.reader.read()).value;
       result.push(...data);
@@ -78,24 +102,24 @@ export default class Client {
     }
   }
 
-  async getVariable(variable) {
-    return await this.command(cmds.GET_VARIABLE, variable.size, variable.id);
+  async #getVariable(variable) {
+    return await this.#command(cmds.GET_VARIABLE, variable.size, variable.id);
   }
 
-  async setVariable(variable, value) {
-    const result = await this.command(cmds.SET_VARIABLE, variable.size, variable.id, value);
-    return result;
+  async #setVariable(variable, value) {
+    return await this.#command(cmds.SET_VARIABLE, variable.size, variable.id, value);
   }
 
-  async identify() {
-    const [ofwPcbVer] = await this.command(cmds.OFW_PCB_VER);
-    const [ofwFwVer] = await this.command(cmds.OFW_FW_VER);
+  async #identify() {
+    const [ofwPcbVer] = await this.#command(cmds.OFW_PCB_VER);
+    const [ofwFwVer] = await this.#command(cmds.OFW_FW_VER);
 
     if ((ofwPcbVer < 5) || (ofwFwVer == 0)) {
       throw new Error("unsupported ofw version", ofwPcbVer, ofwFwVer);
     }
 
-    const [info, nameEnc, cartPowerCtrl, bootloaderReset] = await this.command(cmds.QUERY_FW_INFO);
+    const [info, nameEnc, cartPowerCtrl, bootloaderReset] =
+        await this.#command(cmds.QUERY_FW_INFO);
     const [cfwID, fwVer, pcbVer, fwTs] = unpack("BHBI", info);
     const fwDate = new Date(fwTs * 1000);
     const name = latin1.decode(nameEnc).replaceAll("\u0000", "");
@@ -107,4 +131,22 @@ export default class Client {
 
     return {cfwID, fwVer, pcbVer, fwDate, name, cartPowerCtrl, bootloaderReset};
   }
-};
+
+  async command(cmd, ...args) {
+    return await this.#serialize(async () => await this.#command(cmd, ...args));
+  }
+
+  async transfer(cmd, size, callback, ...args) {
+    return this.#serialize(async () => this.#transfer(cmd, size, callback, ...args));
+  }
+
+  async identify() { return this.#serialize(async () => this.#identify()); }
+
+  async getVariable(variable) {
+    return await this.#serialize(async () => this.#getVariable(variable));
+  }
+
+  async setVariable(variable, value) {
+    return await this.#serialize(async () => this.#setVariable(variable, value));
+  }
+}
